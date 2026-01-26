e4f4614b151d8ee08d2b7725072f068a8c5d9b5c
Serhii Yemets <Serhiiyemets2020@gmail.com>
2026-01-26 18:06:46 +0100

    AuthNavigation file commit

---
+++ components/AuthNavigation/AuthNavigation.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import { useRouter } from "next/router";
+import css from "./AuthNavigation.module.css";
+import { useUserAuthStore } from "@/lib/store/authStore";
+import Link from "next/link";
+
+export default function AuthNavigation() {
+    const router = useRouter();
+    const { isAuthenticated, user, clearIsAuthenticated } = useUserAuthStore();
+
+    const handleLogout = async () => {
+        await clearIsAuthenticated();
+        router.push('/sign-in');
+    };
+
+    return (
+    <>
+        {isAuthenticated ? (
+            <>
+            <li className={css.navigationItem}>
+                <Link
+                    href="/profile"
+                    prefetch={false}
+                    className={css.navigationLink}
+                    >
+                    Profile
+                </Link>
+            </li>
+
+            <li className={css.navigationItem}>
+                <p className={css.userEmail}>{user?.email}</p>
+                <button onClick={handleLogout} className={css.logoutButton}>
+                Logout
+                </button>
+            </li>
+            </>
+        ) : (
+            <>
+            <li className={css.navigationItem}>
+                <Link
+                    href="/sign-in"
+                    prefetch={false}
+                    className={css.navigationLink}
+                    >
+                    Login
+                </Link>
+            </li>
+
+            <li className={css.navigationItem}>
+                <Link
+                    href="/sign-up"
+                    prefetch={false}
+                    className={css.navigationLink}
+                    >
+                    Sign up
+                </Link>
+            </li>
+            </>
+        )}
+        </>
+    );
+}
---
+++ types/user.ts
@@ -0,0 +1,14 @@
+export interface User {
+    username: string;
+    email: string;
+    avatar: string;
+}
+
+export interface LoginRequest {
+    email: string;
+    password: string;
+}
+
+export interface UpdateUser {
+    username: string;
+}
